586495e858aa6a1a188e7284896d4e6b54ff1496
Deguyenne Nicolas <nicolas.deguyenne@etu.univ-orleans.fr>
2022-09-26 10:59:03 +0200

    replace entities props by id

--- todos/src/main/java/fr/hndgy/todos/entities/Project.java
+++ todos/src/main/java/fr/hndgy/todos/entities/Project.java
@@ -4,6 +4,12 @@ import lombok.*;
 
 import javax.persistence.*;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,6 +22,11 @@ public class Project {
     private String name;
     private String description;
 
+    @JsonIgnore
     @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id")
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+    @JsonIdentityReference(alwaysAsId = true)
+    @JsonProperty("userId")
     private AppUser createdBy;
 }
--- todos/src/main/java/fr/hndgy/todos/entities/Todo.java
+++ todos/src/main/java/fr/hndgy/todos/entities/Todo.java
@@ -6,6 +6,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import java.time.LocalDateTime;
 
 @Entity
@@ -26,9 +32,17 @@ public class Todo {
     private boolean isPriority;
 
     @ManyToOne
+    @JoinColumn(name = "project_id")
+    @JsonIdentityInfo(property = "id", generator = ObjectIdGenerators.PropertyGenerator.class)
+    @JsonIdentityReference(alwaysAsId = true)
+    @JsonProperty("projectId")
     private Project project;
 
     @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonIdentityInfo(property = "id", generator = ObjectIdGenerators.PropertyGenerator.class)
+    @JsonIdentityReference(alwaysAsId = true)
+    @JsonProperty("userId")
     private AppUser createdBy;
 
 }
